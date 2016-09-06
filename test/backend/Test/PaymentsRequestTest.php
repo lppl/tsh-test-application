c6d42d4e83c27967a9dda2a5fdd4d4efbce5aedd
Lukasz Pietrek <lpietrek@gmail.com>
2016-09-06 12:53:03 +0200

    Fix security issue - misspell

--- test/backend/Test/PaymentsRequestTest.php
+++ test/backend/Test/PaymentsRequestTest.php
@@ -19,7 +19,7 @@ class PaymentsRequestTest extends TestCase
     {
         $request = new PaymentsRequest(1, '"><script>alert("jaka_piekna_wtopa")</script><"', 3);
         self::assertSame(
-            '&#34;&#62;&#60;script&#62;alert(&#34;jaka_piekna_wtopa&#34;)&#60;/script&#62;&#60;&#34',
+            '&#34;&#62;&#60;script&#62;alert(&#34;jaka_piekna_wtopa&#34;)&#60;/script&#62;&#60;&#34;',
             $request->supplier()
         );
     }
